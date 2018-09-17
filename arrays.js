7f726d7d802c65162b6d409fb02736202a4b8e3e
Katie G <oharagolden@gmail.com>
2018-09-17 00:28:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,6 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-var cities = ["New York", "San Francisco"]
-
-cities.unshift("Philadelphia")
-
-var cities = ["New York", "San Francisco"]
- 
-// we can assign it to the existing `cities` variable
-cities = ["Philadelphia", ...cities]
-
+function addElementToEndOfArray(a, b) {
+  
+}
+function destructivelyAddElementToEndOfArray(a, b) {
+  
+}
