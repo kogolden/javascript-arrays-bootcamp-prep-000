90ba1f1e70ef330ac934d8f3ca1c593b837d765a
Katie G <oharagolden@gmail.com>
2018-09-17 00:30:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,5 @@ function destructivelyAddElementToEndOfArray(a, b) {
 var entrepreneurs = ["Elizabeth Holmes", "Laurene Powell Jobs", "Arianna Huffington"];
  
 console.log(entrepreneurs[0]);
+var bio = " is the co-founder and editor-in-chief of The Huffington Post";
+console.log(entrepreneurs[2] + bio);
