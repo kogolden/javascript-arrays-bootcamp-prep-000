a49f88c45973908dcb5cf9888dcfc4c8aacd26a6
Katie G <oharagolden@gmail.com>
2018-09-17 02:09:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function removeElementFromBeginningOfArray(Array) {
   return Array
 }
 function destructivelyRemoveElementFromEndOfArray(Array) {
-  Array = Array.pop(-1)
+  Array = Array.pop()
   return Array
 }
 function removeElementFromEndOfArray(Array) {
