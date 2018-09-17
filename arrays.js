58f5afeb45c0fe144e520120288a8752976ac869
Katie G <oharagolden@gmail.com>
2018-09-17 00:28:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ function addElementToEndOfArray(a, b) {
 function destructivelyAddElementToEndOfArray(a, b) {
   
 }
+var entrepreneurs = ["Elizabeth Holmes", "Laurene Powell Jobs", "Arianna Huffington"];
+ 
+// the line below will print the string "Elizabeth Holmes"
+console.log(entrepreneurs[0]);
