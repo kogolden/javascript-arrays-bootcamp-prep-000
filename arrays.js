64032786516045374092c9a76abfeaa1215aeadc
Katie G <oharagolden@gmail.com>
2018-09-17 00:36:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
 function addElementToEndOfArray(a, b) {
   
 }
 function destructivelyAddElementToEndOfArray(a, b) {
   
 }
-var entrepreneurs = ["Elizabeth Holmes", "Laurene Powell Jobs", "Arianna Huffington"];
- 
-console.log(entrepreneurs[0]);
-var bio = " is the co-founder and editor-in-chief of The Huffington Post";
-console.log(entrepreneurs[2] + bio);
+function accessElementToEndOfArray(array, index) {
+  
+}
